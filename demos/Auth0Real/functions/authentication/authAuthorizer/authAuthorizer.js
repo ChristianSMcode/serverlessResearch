f0d845b8339c75f7da2cb61f9b66186a9b3134a2
Christian David <cris920sdm@hotmail.com>
2023-04-03 20:03:57 -0500

    Independant auth demo

---
+++ demos/Auth0Real/functions/authentication/authAuthorizer/authAuthorizer.js
@@ -0,0 +1,14 @@
+const tokenProcessing = require('./tokenProcessing');
+let data;
+
+// Lambda function index.handler - thin wrapper around lib.authenticate
+exports.lambdaHandler  = async (event, context) => {
+  try {
+    data = await tokenProcessing.authenticate(event);
+  }
+  catch (err) {
+      console.log(err.message);
+      return context.fail("Unauthorized");
+  }
+  return data;
+};
