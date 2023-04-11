0792cdcb9b5c5f9f402e38592e9e20ef4dfd46f2
Christian David <cris920sdm@hotmail.com>
2023-04-11 11:08:46 -0500

    solve cors error on unauthenticated scenario

--- demos/Auth0Real/auth0FrontSimulation/main.js
+++ demos/Auth0Real/auth0FrontSimulation/main.js
@@ -1,6 +1,12 @@
 const apigClient = apigClientFactory.newClient();
 
 const log = document.querySelector('.log');
+const nameInput = document.querySelector('.nameInput');
+const passwordInput = document.querySelector('.passwordInput');
+const resultData = document.querySelector('.resultData');
+const testAction = document.querySelector('.testAction');
+const statusSet = document.querySelector('.status');
+//LogUser-Save token
 log.addEventListener('click',()=>{
     let params = {};
     let additionalParams = {
@@ -9,15 +15,43 @@ log.addEventListener('click',()=>{
     } 
 
     let body ={
-        "email": "david.sierra@gmail.com",
-        "password": "r1v2l3P4B5O667.."
+        "email": nameInput.value,
+        "password": passwordInput.value
     }
 
     apigClient.usersLogInPost(params,body,additionalParams)
         .then( (result) => {
-            console.log(result)})
+            console.log(result)
+            let access_token = result.data.access_token
+            resultData.innerHTML = access_token.slice(0,10) + '...'
+            localStorage.setItem("access_token",access_token)
+        })
         .catch((err)=>{
+            resultData.innerHTML = JSON.stringify(result.data)
             console.log(err)
         })
 
+});
+
+testAction.addEventListener('click',()=>{
+    let acces_token = localStorage.getItem("access_token");
+    let params = {};
+    let additionalParams = {
+        headers:{
+            'Authorizationtoken':'Bearer ' + acces_token
+        },
+        queryParams:{}
+    } 
+    let body ={};
+
+    apigClient.actionsAuth0Get(params,body,additionalParams)
+    .then( (result) => {
+        console.log(result);
+        statusSet.innerHTML = 'Authenticated';
+
+    })
+    .catch((err)=>{
+        console.log(err);
+        statusSet.innerHTML = 'Error aunthenticating';
+    })
 })
